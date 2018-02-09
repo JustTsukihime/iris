1733a2f271eadde6bd3f36ba751c63822bcaabb8
Deniss Celuiko <deniss.celuiko@gmail.com>
2018-02-09 14:16:30 +0200

    Try #2

--- app/Http/Controllers/MattermostSlashCommandTest.php
+++ app/Http/Controllers/MattermostSlashCommandTest.php
@@ -8,17 +8,18 @@ class MattermostSlashCommandTest extends Controller
 {
     function test(Request $request)
     {
-        $this->validate($request, [
-            'channel_id' => 'required',
-            'channel_name' => 'required',
-            'command' => 'required',
-            'team_domain' => 'required',
-            'team_id' => 'required',
-            'text' => 'required',
-            'token' => 'required',
-            'user_id' => 'required',
-            'user_name' => 'required',
-        ]);
+//        $this->validate($request, [
+//            'channel_id' => 'required',
+//            'channel_name' => 'required',
+//            'command' => 'required',
+//            'team_domain' => 'required',
+//            'team_id' => 'required',
+//            'text' => 'required',
+//            'token' => 'required',
+//            'user_id' => 'required',
+//            'user_name' => 'required',
+//        ]);
+
         $res_text = $request->user_name.' tries using a slash command! \n They said: "'.$request->text.'"';
 
         return [
